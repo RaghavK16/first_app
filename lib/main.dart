676ccd0c76a431cf132996869626b9f10b8fb574
Raghav Khullar <42183326+RaghavK16@users.noreply.github.com>
2019-10-17 09:37:11 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'location_details.dart';
+import 'mocks/mock_location.dart';
+import 'location_list.dart';
+
+void main() {
+  final mockLocations = MockLocation.fetchAll();
+
+  return runApp(MaterialApp(home: LocationList(mockLocations)));
+}
